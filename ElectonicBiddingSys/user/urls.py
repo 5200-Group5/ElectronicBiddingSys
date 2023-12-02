558904ba9d411079e5f5dc424b7ca4d5d72afa66
daisylin927 <daisylin927@gmail.com>
2023-12-02 13:23:50 -0800

    change landing page

--- ElectonicBiddingSys/user/urls.py
+++ ElectonicBiddingSys/user/urls.py
@@ -6,8 +6,8 @@ from . import views
 app_name = "user"
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("login/", views.UserLoginView.as_view(), name="login"),
     path("register/", views.UserRegisterView.as_view(), name="register"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
