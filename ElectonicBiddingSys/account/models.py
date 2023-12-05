df41026eafe6c6a7be055bbd6d7d2f783910a6ac
Miao Yuan <miaoyuan343@gmail.com>
2023-12-05 01:37:07 -0800

    add report function

--- ElectonicBiddingSys/account/models.py
+++ ElectonicBiddingSys/account/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ReportedIssue(models.Model):
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        db_table = 'message'
+    def __str__(self):
+        return f'ReportedIssue {self.id}'
