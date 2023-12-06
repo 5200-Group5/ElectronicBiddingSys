d4ccf36db50afafe30dc8cc7b4d1f33cfeb717cd
HypnosNi <zeng.kew@northeastern.edu>
2023-12-05 16:19:18 -0800

    deploy

--- ElectonicBiddingSys/ElectonicBiddingSys/settings.py
+++ ElectonicBiddingSys/ElectonicBiddingSys/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-=_=ap4dxgxnp18d8mif(n$xy!t0lsn&r+&_=lcrg8w89p9x0$@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
